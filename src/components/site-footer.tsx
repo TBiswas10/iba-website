afc72e2443166d1e020b405109e192efd4ff88e8
Tito <tirtha.biswas@education.nsw.gov.au>
2026-04-16 19:33:59 +1000

    Style navbar and footer: center nav items, increase spacing, add teal border and quick links to footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -5,27 +5,32 @@ export function SiteFooter() {
         <div className="footer-brand">
           <h3>Illawarra Bengali Association</h3>
           <p>Community, culture, care, and connection across generations.</p>
+          <p className="tagline">Join the Community</p>
           <p className="copyright">&copy; Made by Tirtha Biswas and Arnab Biswas</p>
         </div>
         <div>
-          <h4>Social</h4>
+          <h4>Quick Links</h4>
           <ul>
-            <li>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer">
-                Facebook
-              </a>
-            </li>
-            <li>
-              <a href="https://instagram.com" target="_blank" rel="noreferrer">
-                Instagram
-              </a>
-            </li>
-            <li>
-              <a href="https://youtube.com" target="_blank" rel="noreferrer">
-                YouTube
-              </a>
-            </li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/events">Events</a></li>
+            <li><a href="/membership">Membership</a></li>
+            <li><a href="/resources">Resources</a></li>
           </ul>
+        </div>
+        <div>
+          <h4>Social</h4>
+          <div className="social-links">
+            <a href="https://facebook.com" target="_blank" rel="noreferrer" className="social-link">
+              Facebook
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer" className="social-link">
+              Instagram
+            </a>
+            <a href="https://youtube.com" target="_blank" rel="noreferrer" className="social-link">
+              YouTube
+            </a>
+          </div>
         </div>
         <div>
           <h4>Contact</h4>
