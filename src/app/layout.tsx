372655d8cccfd97bb164016bf47f98660dc0fd9b
Tito <tirtha.biswas@education.nsw.gov.au>
2026-04-19 21:33:14 +1000

    feat: add Google Site Verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,7 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <head>
+        <meta name="google-site-verification" content="UL4C9a9xEBsiqC-Fbog3btbkUNw2dlvKHxNtsuYEKOI" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
