406a31bc59a4869a78c8dbaacf2d68750578ecf3
Tito <tirtha.biswas@education.nsw.gov.au>
2026-04-21 14:41:58 +1000

    fix(qa): remove trailingSlash to prevent 308 redirects

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
+  trailingSlash: false,
   allowedDevOrigins: ["10.0.0.72"],
   images: {
     remotePatterns: [
